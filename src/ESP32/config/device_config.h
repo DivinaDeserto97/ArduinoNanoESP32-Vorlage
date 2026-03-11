5ccfbc726402b0b955c4fbdf6d4cb26e2e6e819f
DivinaDeserto97 <divina.deserto20@gmail.com>
2026-03-11 11:45:34 +0100

    Test OTA

--- src/ESP32/config/device_config.h
+++ src/ESP32/config/device_config.h
@@ -16,8 +16,8 @@
 #define STATUS_ROUTE "/status"
 
 // LED Zeiten
-#define LED_ON_TIME_MS 500
-#define LED_OFF_TIME_MS 1000
+#define LED_ON_TIME_MS 200
+#define LED_OFF_TIME_MS 400
 
 // WLAN Überprüfung Intervall
 #define WIFI_CHECK_INTERVAL_MS 10000
